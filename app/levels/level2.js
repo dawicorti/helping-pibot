72ab288ab334ff312a3cb1cca4ad7f003da1e5b0
dawicorti <david@mediaodyssee.com>
2012-11-27 23:18:12 +0100

    add lost levels

---
+++ app/levels/level2.js
@@ -0,0 +1,22 @@
+/*global define,document*/
+/*jslint nomen: true*/
+
+define(function (require) {
+    "use strict";
+
+    return {
+        chunks: [
+            {"pos": {"y": 0.0, "x": 0.0}, "name": "staticbox", "options": {"width": 6.0, "height": 1.0}},
+            {"pos": {"y": -1.0, "x": 3.5250000000000004}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": -2.0000003750000004, "x": 4.65}, "name": "staticbox", "options": {"width": 1.25, "height": 1.0}},
+            {"pos": {"y": -1.0, "x": 5.775}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": 0.0, "x": 8.775}, "name": "staticbox", "options": {"width": 5.0, "height": 1.0}},
+            {"pos": {"y": -2.0000003750000004, "x": 12.025}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": -2.0000003750000004, "x": 17.275000000000002}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": -2.750000375000001, "x": 18.525000000000002}, "name": "staticbox", "options": {"width": 1.5, "height": 0.5}},
+            {"pos": {"y": -1.5, "x": 20.525000000000002}, "name": "staticbox", "options": {"width": 2.5, "height": 3.0}},
+            {"pos": {"y": -2.0000003750000004, "x": 18.525000000000002}, "name": "teleporter", "options": {"width": 1.0, "height": 1.0}}
+        ]
+    };
+
+});
---
+++ app/levels/level3.js
@@ -0,0 +1,25 @@
+/*global define,document*/
+/*jslint nomen: true*/
+
+define(function (require) {
+    "use strict";
+
+    return {
+        chunks: [
+            {"pos": {"y": 0.0, "x": 0.0}, "name": "staticbox", "options": {"width": 6.0, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 3.5}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 13.0}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 17.75}, "name": "staticbox", "options": {"width": 8.5, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 23.25}, "name": "staticbox", "options": {"width": 2.5, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 29.75}, "name": "staticbox", "options": {"width": 7.5, "height": 1.0}},
+            {"pos": {"y": -5.9995273749999996, "x": 25.25}, "name": "staticbox", "options": {"width": 1.5, "height": 1.0}},
+            {"pos": {"y": 57.000471125000004, "x": 3.0}, "name": "rigidbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": 55.000471125000004, "x": 4.0}, "name": "rigidbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": 51.500471125000004, "x": 3.5}, "name": "rigidbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": -5.9995273749999996, "x": 34.25}, "name": "staticbox", "options": {"width": 1.5, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 40.0}, "name": "staticbox", "options": {"width": 10.0, "height": 1.0}},
+            {"pos": {"y": 1.0004726250000004, "x": 46.25}, "name": "staticbox", "options": {"width": 2.5, "height": 5.0}}
+        ]
+    };
+
+});
