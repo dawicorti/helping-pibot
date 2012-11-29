f09733bc7ae1c3f9216e0a7c00f4ca24de7b76a9
dawicorti <david@mediaodyssee.com>
2012-11-29 14:05:13 +0100

    level 5

---
+++ app/levels/level5.js
@@ -0,0 +1,34 @@
+/*global define,document*/
+/*jslint nomen: true*/
+
+define(function (require) {
+    "use strict";
+
+    return {
+        chunks: [
+            {"pos": {"y": 0.0, "x": 0.0}, "name": "staticbox", "options": {"width": 6.0, "height": 1.0}},
+            {"pos": {"y": 4.750472625, "x": -0.5}, "name": "staticbox", "options": {"width": 2.0, "height": 8.5}},
+            {"pos": {"y": 8.500472625, "x": 12.5}, "name": "staticbox", "options": {"width": 24.0, "height": 1.0}},
+            {"pos": {"y": 2.125472625, "x": 16.875}, "name": "platform", "options": {"width": 1.25, "velocity": 0.5, "target": {"y": 5.1254726250000004, "x": 16.875}, "identity": 0, "height": 3.25}},
+            {"pos": {"y": 5.5004725875, "x": 43.75}, "name": "platform", "options": {"width": 3.0, "velocity": 0.01, "target": {"y": 5.5004725875, "x": -156.25}, "identity": 1, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 27.0}, "name": "switch", "options": {"width": 1.0, "identity": 0, "height": 1.0}},
+            {"pos": {"y": -1.9995273749999996, "x": 27.0}, "name": "staticbox", "options": {"width": 2.0, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 38.25}, "name": "switch", "options": {"width": 1.0, "identity": 1, "height": 1.0}},
+            {"pos": {"y": -1.9995273749999996, "x": 38.25}, "name": "staticbox", "options": {"width": 1.5, "height": 1.0}},
+            {"pos": {"y": 4.250472625, "x": 48.75}, "name": "staticbox", "options": {"width": 1.0, "height": 9.5}},
+            {"pos": {"y": -0.9995273750000003, "x": 8.0}, "name": "staticbox", "options": {"width": 1.5, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 5.125}, "name": "staticbox", "options": {"width": 4.25, "height": 1.0}},
+            {"pos": {"y": 6.5004726250000004, "x": 7.0}, "name": "staticbox", "options": {"width": 1.0, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 21.625}, "name": "staticbox", "options": {"width": 9.5, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 12.8125}, "name": "staticbox", "options": {"width": 8.125, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 26.25}, "name": "staticbox", "options": {"width": 0.5, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 27.75}, "name": "staticbox", "options": {"width": 0.5, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 32.5625}, "name": "staticbox", "options": {"width": 10.125, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 37.625}, "name": "staticbox", "options": {"width": 0.25, "height": 1.0}},
+            {"pos": {"y": -0.9995273750000003, "x": 38.875}, "name": "staticbox", "options": {"width": 0.25, "height": 1.0}},
+            {"pos": {"y": 0.00047262499999973785, "x": 43.5}, "name": "staticbox", "options": {"width": 9.5, "height": 1.0}},
+            {"pos": {"y": 1.0004726249999998, "x": 46.375}, "name": "teleporter", "options": {"width": 0.5, "height": 0.5}}
+        ]
+    };
+
+});
